a87b2c0ba565f72fa964e750624b980da5d81b76
YemingLakeForest <ansonn2005@hotmail.com>
2015-12-06 20:53:52 +0000

    Used a path finder to locate parent folder ID instead of hard code.

--- src/main/java/com/lf/bom/dao/GoogleDriveCommitDao.java
+++ src/main/java/com/lf/bom/dao/GoogleDriveCommitDao.java
@@ -4,13 +4,15 @@ import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.ParentReference;
+import com.lf.bom.helper.GoogleDrivePathFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Component("googleDriveCommitDao")
@@ -20,6 +22,15 @@ public class GoogleDriveCommitDao implements CommitDao {
 
     @Autowired
     private Drive service;
+
+    @Value("${google.drive.backup.path}")
+    private String path;
+
+    @Value("${google.backup.mime.type}")
+    private String mimeType;
+
+    @Autowired
+    private GoogleDrivePathFinder pathFinder;
 
     @Override
     public void commit(List<String> filePaths) {
@@ -36,16 +47,19 @@ public class GoogleDriveCommitDao implements CommitDao {
         File body = new File();
         body.setTitle(fileName);
         body.setDescription(fileName);
-        body.setMimeType("text/x-sql");
+        body.setMimeType(mimeType);
 
-        body.setParents(Arrays.asList(new ParentReference().setId("0Bx_DfLBtRih8QTI2M0dESTRyc1E")));
-
-        java.io.File fileContent = new java.io.File(filePath);
-        FileContent mediaContent = new FileContent("text/x-sql", fileContent);
         try {
+
+            String parentId = pathFinder.findPathId(path);
+
+            body.setParents(Collections.singletonList(new ParentReference().setId(parentId)));
+
+            java.io.File fileContent = new java.io.File(filePath);
+            FileContent mediaContent = new FileContent(mimeType, fileContent);
             File file = service.files().insert(body, mediaContent).execute();
 
-            LOGGER.info("Sent file: ", file.getId());
+            LOGGER.info("Sent file: ", file.getTitle());
 
         } catch (IOException e) {
             LOGGER.error("An error occured: " + e, e);
